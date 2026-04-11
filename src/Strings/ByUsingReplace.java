b28b75465c532a156235023cec917526641d0bca
Sasi Kumar <sasi7778kumar@gmail.com>
2026-04-12 00:05:27 +0530

    String

---
+++ src/Strings/ByUsingReplace.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class ByUsingReplace {
+	public static void main(String args[]) {
+		String str1 = "I Love Food";
+		System.out.println(str1.replace("Love", "Hate"));
+	}
+}
---
+++ src/Strings/ComparetheString.java
@@ -0,0 +1,18 @@
+package Strings;
+import java.util.*;
+public class ComparetheString {
+	public static void main(String args[]) {
+		String str[] = {"Grapes","Kiwi","Orange","Banana","lemon","Apple"};
+		
+		for(int i=0;i<str.length-1;i++) {
+			for(int j=0;j<str.length-1;j++) {
+				if(str[j].compareTo(str[j+1])>0) {
+						String temp = str[j];
+						str[j] = str[j+1];
+						str[j+1] = temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(str));
+	}
+}
