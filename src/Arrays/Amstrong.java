93b649b1c9d3b0518cc47e9c2b1792d583a2fda3
Sasi Kumar <sasi7778kumar@gmail.com>
2026-04-13 14:58:49 +0530

    Amstrong

--- src/Arrays/Amstrong.java
+++ src/Arrays/Amstrong.java
@@ -1,7 +1,36 @@
 package Arrays;
 import java.util.*;
 public class Amstrong {
-	public static void main(String args[]) {
+	static Scanner sc = new Scanner(System.in);
+	public static String CheckAmstrongNumber(int num) {
+		int count =0;
+		int temp = num,org = num;
+		int ans =0;
+
+		while(num >0) {
+			int a = num % 10;
+			count++;
+			num /= 10;
+		}
+		int sum =0;
+		while(temp >0) {
+			int a = temp%10;
+			System.out.println(a);
+			sum =sum + Math.powExact(a,count);
+//			ans+=sum;
+			System.out.println(sum);
+			temp /=10;
+			
+		}
+		System.out.println(ans);
+		if(sum == org )
+			return "Its Amstrong Number";
 		
+		return "Its Not a Amstrong";		
+	}
+	public static void main(String args[]) {
+		System.out.print("Enter the number");
+		int num = sc.nextInt();
+		System.out.println(CheckAmstrongNumber(num));
 	}
 }
