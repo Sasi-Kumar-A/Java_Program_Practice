06fd3f0fe0544bf556a449cedcd580ff6375968f
Sasi Kumar <sasi7778kumar@gmail.com>
2026-05-13 18:51:28 +0530

    Missing Numbers

--- src/Arrays/MissingNumbers.java
+++ src/Arrays/MissingNumbers.java
@@ -7,15 +7,22 @@ public class MissingNumbers {
 		int n = sc.nextInt();
 		int arr[] = new int[n];
 		
-		for(int i=0;i<n;i++) {
+		for(int i=0;i<n;i++)
 			arr[i] = sc.nextInt();
+		
+		
+		System.out.print("The Missing Number is: ");
+		for(int i=0;i<n-1;i++) {
+			for(int j=arr[i]+1;j<arr[i+1];j++) {
+				System.out.print(j+" ");
+			}
 		}
 		
-		System.out.print("The Missing Numbers are:  ");
-		for(int i=1;i<n;i++) {
-			if(arr[i] != i)
-				System.out.print(i);
-		}
+//		System.out.print("The Missing Numbers are:  ");
+//		for(int i=0;i<n;i++) {
+//			if(arr[i] != i+1)
+//				System.out.print(i+1+" ");
+//		}
 	}
 
 }
