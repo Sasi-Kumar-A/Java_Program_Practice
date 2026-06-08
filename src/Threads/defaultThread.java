972951406bf078e544cbcbe47692cea2c1abea49
Sasi Kumar <sasi7778kumar@gmail.com>
2026-06-08 23:34:24 +0530

    Default Thread

---
+++ src/Threads/defaultThread.java
@@ -0,0 +1,10 @@
+package Threads;
+
+public class defaultThread {
+	
+	public static void main(String[] args) {
+		System.out.println("Haii Babssss  "+Thread.currentThread().getName());
+		System.out.println("Yanga da irukaaaa  "+Thread.currentThread().getId());
+		System.out.println("Naaa Mukiyama illa un friend mukiyama daaa  "+Thread.currentThread().getPriority());
+	}
+}
