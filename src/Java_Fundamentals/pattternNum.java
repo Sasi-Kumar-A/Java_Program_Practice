cd989ef399ba0fd78ea434347f640e27a0832967
Sasi Kumar <sasi7778kumar@gmail.com>
2026-06-10 23:42:52 +0530

    Pattern

---
+++ src/Java_Fundamentals/pattternNum.java
@@ -0,0 +1,25 @@
+package Java_Fundamentals;
+import java.util.*;
+public class pattternNum {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+//		int a = sc.nextInt();
+//		int ele =1;
+//		for(int i=1;i<=a;i++) {
+//			for(int j=1;j<=ele;j++) {
+//				System.out.print(i+" ");
+//			
+//			}
+//			System.out.println();
+//			if(i<a/2) {
+//				ele++;
+//			}
+//			else if(i == a/2) {
+//				continue;
+//			}
+//			else
+//				ele--;
+//		}
+		
+	}
+}
