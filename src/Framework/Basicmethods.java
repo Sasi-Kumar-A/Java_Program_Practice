9b9744750752f5cee9b601271384b5230b8e4373
Sasi Kumar <sasi7778kumar@gmail.com>
2026-07-05 22:42:14 +0530

    Basic

---
+++ src/Framework/Basicmethods.java
@@ -0,0 +1,20 @@
+package Framework;
+import java.util.*;
+
+public class Basicmethods {
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		
+		al.add(10);
+		al.add("Ram");
+		al.add(23.54);
+		al.add('A');
+		al.add(true);
+		al.add(1052);
+		al.add(null);
+		al.add(10);
+		
+		al.remove(0);
+	}
+}
