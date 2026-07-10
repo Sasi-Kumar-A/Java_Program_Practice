740cc2ab11850f0a84b25720465bb4d02bcabc1c
Sasi Kumar <sasi7778kumar@gmail.com>
2026-07-10 21:24:52 +0530

    1to100

---
+++ src/Framework/Include1to100.java
@@ -0,0 +1,14 @@
+package Framework;
+import java.util.*;
+public class Include1to100 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> li = new ArrayList<>();
+		
+		for(int i=0;i<100;i++) {
+			li.add(i);
+		}
+		
+		System.out.println(li);
+	}
+}
