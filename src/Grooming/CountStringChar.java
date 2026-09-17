d549deee0c8ab0c431c06985ba20b8ec15124f41
Sasi Kumar <sasi7778kumar@gmail.com>
2026-09-17 23:34:12 +0530

    Count String

---
+++ src/Grooming/CountStringChar.java
@@ -0,0 +1,24 @@
+package Grooming;
+
+public class CountStringChar {
+
+	public static void main(String[] args) {
+		String s1 = "aaabbcccdd";
+		String s = "";
+		
+		char arr[] = s1.toCharArray();
+		for(int i=0;i<arr.length;i++) {
+			int count = 1;
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[i] == arr[j]) {
+					count++;
+					arr[j] = '0';
+				}
+			}
+			if(arr[i] != '0') {
+				s = s+arr[i]+count;
+			}
+		}
+		System.out.println(s);
+	}
+}
