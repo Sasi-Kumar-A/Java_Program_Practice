e0ee879712045137628ce9ee733af3ee15e8ddef
Sasi Kumar <sasi7778kumar@gmail.com>
2026-09-18 15:27:39 +0530

    Group Anagram

---
+++ src/Grooming/GroupAnagram.java
@@ -0,0 +1,21 @@
+package Grooming;
+
+import java.util.*;
+
+public class GroupAnagram {
+
+	public static void main(String[] args) {
+		String[] strs = {"eat","tea","tan","ate","nat","bat"};
+
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String s : strs){
+            char arr[] = s.toCharArray();
+            Arrays.sort(arr);
+
+            String key = new String(arr);
+            map.putIfAbsent(key,new ArrayList<>());
+            map.get(key).add(s);
+        }
+        System.out.println(map.values());
+	}
+}
